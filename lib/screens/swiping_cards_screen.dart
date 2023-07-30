57ff705efc1916da5b0cb9d6cdfc05339486d274
camelcrush <dkxm0927@naver.com>
2023-07-30 20:43:19 +0900

    AnimationController for Drag

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -7,46 +7,63 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
-  double posX = 0;
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 2),
+    // 기본적으로 AnimationController의 값 범위는 [0,1]이므로 아래와 같이 설정해줘야 함
+    upperBound: size.width,
+    lowerBound: size.width * -1,
+    value: 0,
+  );
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      posX += details.delta.dx;
-    });
+    _animationController.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    setState(() {
-      posX = 0;
-    });
+    _animationController.animateTo(
+      0,
+      curve: Curves.bounceOut,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    print(posX);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Swiping Cards"),
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: GestureDetector(
-          onHorizontalDragUpdate: _onHorizontalDragUpdate,
-          onHorizontalDragEnd: _onHorizontalDragEnd,
-          child: Transform.translate(
-            offset: Offset(posX, 0),
-            child: Material(
-              elevation: 10,
-              color: Colors.red.shade100,
-              child: SizedBox(
-                width: size.width * 0.8,
-                height: size.height * 0.5,
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd: _onHorizontalDragEnd,
+                  child: Transform.translate(
+                    offset: Offset(_animationController.value, 0),
+                    child: Material(
+                      elevation: 10,
+                      color: Colors.red.shade100,
+                      child: SizedBox(
+                        width: size.width * 0.8,
+                        height: size.height * 0.5,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
+            ],
+          );
+        },
       ),
     );
   }
