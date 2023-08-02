ba78bebcbabb52dd291549a287c7531584c9acad
camelcrush <dkxm0927@naver.com>
2023-08-02 20:26:24 +0900

    Album PageView: PageController, PageView.builder()

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animations/screens/apple_watch_screen.dart';
 import 'package:flutter_animations/screens/explicit_animations_screen.dart';
 import 'package:flutter_animations/screens/implicit_animations_screen.dart';
+import 'package:flutter_animations/screens/music_player.screen.dart';
 import 'package:flutter_animations/screens/swiping_cards_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -22,46 +23,56 @@ class MenuScreen extends StatelessWidget {
         title: const Text("Flutter Animations"),
       ),
       body: Center(
-          child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              _goToPage(
-                context,
-                const ImplicitAnimationsScreen(),
-              );
-            },
-            child: const Text("Implicit Animations"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _goToPage(
-                context,
-                const ExplicitAnimationsScreen(),
-              );
-            },
-            child: const Text("Explicit Animations"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _goToPage(
-                context,
-                const AppleWatchScreen(),
-              );
-            },
-            child: const Text("Apple Watch"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _goToPage(
-                context,
-                const SwipingCardsScreen(),
-              );
-            },
-            child: const Text("Swiping Cards"),
-          ),
-        ],
-      )),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const ImplicitAnimationsScreen(),
+                );
+              },
+              child: const Text("Implicit Animations"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const ExplicitAnimationsScreen(),
+                );
+              },
+              child: const Text("Explicit Animations"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const AppleWatchScreen(),
+                );
+              },
+              child: const Text("Apple Watch"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const SwipingCardsScreen(),
+                );
+              },
+              child: const Text("Swiping Cards"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const MusicPlayerScreen(),
+                );
+              },
+              child: const Text("Music Player"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
