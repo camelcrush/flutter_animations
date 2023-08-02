afad050355f2c3fbafd7853e376e8c6739dfaabb
camelcrush <dkxm0927@naver.com>
2023-08-02 21:14:49 +0900

    AnimatedSwitcher, BackdropFilter

--- lib/screens/music_player.screen.dart
+++ lib/screens/music_player.screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -13,12 +15,53 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     viewportFraction: 0.8,
   );
 
+  int _currentPage = 0;
+
+  void _onChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          // AnimatedSwitcher : child가 변할 때마다 애니메이션을 실행해주는 위젯
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              // Container가 완전히 봐뀌는 것이 아니라서(배경만 바뀜) 고유 key를 부여해줘야
+              // flutter가 변화를 인지하고 AnimatedSwitcher가 애니메이션을 실행함
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/covers/${_currentPage + 1}.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              // BackdropFilter : Child를 대상으로 filter를 적용해주는 위젯
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 20,
+                  sigmaY: 20,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
           PageView.builder(
+            onPageChanged: _onChanged,
             controller: _pageController,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
@@ -31,18 +74,34 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     decoration: BoxDecoration(
                       image: DecorationImage(
                         image: AssetImage("assets/covers/${index + 1}.jpg"),
+                        fit: BoxFit.cover,
                       ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: const Offset(2, 6),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(20),
                     ),
                   ),
                   const SizedBox(height: 30),
                   const Text(
                     "Interstellar",
-                    style: TextStyle(fontSize: 26),
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
                   ),
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 5),
                   const Text(
                     "Hans Zimmer",
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
                   )
                 ],
               );
