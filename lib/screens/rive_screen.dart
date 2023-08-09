6a546b1a5e81b666ba21603b78d935fa079fc9bf
camelcrush <dkxm0927@naver.com>
2023-08-09 23:46:54 +0900

    Rive: Custom Animations

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -37,24 +39,82 @@ class _RiveScreenState extends State<RiveScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Rive"),
-      ),
-      body: Center(
-        child: Container(
-          color: const Color(0xFFFF2ECC),
-          width: double.infinity,
-          child: RiveAnimation.asset(
-            "assets/animations/stars-animation.riv",
-            // artboard: 캔버스
-            artboard: "artboard",
-            // stateMachines: 가지고 있는 Animation set
-            stateMachines: const ["state"],
-            // Controller 초기화 funtion
-            onInit: _onInit,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            width: size.width,
+            height: size.height / 2,
+            child: const RiveAnimation.asset(
+              "assets/animations/balls-animation.riv",
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          Positioned(
+            top: 0,
+            width: size.width,
+            height: size.height / 2,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 50,
+                sigmaY: 50,
+              ),
+              child: const Center(
+                widthFactor: 1,
+                heightFactor: 1,
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: (size.height / 2) - 270,
+            width: size.width,
+            height: size.height / 2,
+            child: const RiveAnimation.asset(
+              "assets/animations/custom-button-animation.riv",
+              stateMachines: ["state"],
+            ),
+          ),
+          Positioned(
+            top: (size.height / 2) - 270,
+            width: size.width,
+            height: size.height / 2,
+            child: const Center(
+                child: Text(
+              "Log in",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+          ),
+          Positioned(
+            top: size.height / 2,
+            width: size.width,
+            height: size.height / 2,
+            child: Center(
+              child: Container(
+                color: const Color(0xFFFF2ECC),
+                width: double.infinity,
+                child: RiveAnimation.asset(
+                  "assets/animations/stars-animation.riv",
+                  // artboard: 캔버스
+                  artboard: "artboard",
+                  // stateMachines: 가지고 있는 Animation set
+                  stateMachines: const ["state"],
+                  // Controller 초기화 funtion
+                  onInit: _onInit,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
